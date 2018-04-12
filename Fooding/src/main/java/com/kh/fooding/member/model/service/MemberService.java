6b9a402db6e8b529704c5e24d69d785b56a33285
user2 <user2@KH_C>
2018-04-12 21:29:50 +0900

    관리자 페이지 기능 추가중

--- Fooding/src/main/java/com/kh/fooding/member/model/service/MemberService.java
+++ Fooding/src/main/java/com/kh/fooding/member/model/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.kh.fooding.member.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
 
 import com.kh.fooding.member.model.exception.LoginException;
@@ -10,5 +12,7 @@ public interface MemberService {
 	Member loginMember(Member m) throws LoginException;
 
 	void insertMember(Member m);
+
+	ArrayList<Member> selectMemberList();
 
 }
--- Fooding/src/main/java/com/kh/fooding/member/model/service/MemberServiceImpl.java
+++ Fooding/src/main/java/com/kh/fooding/member/model/service/MemberServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kh.fooding.member.model.service;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,14 +17,28 @@ public class MemberServiceImpl implements MemberService{
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
+	// 로그인
 	@Override
-	public Member loginMember(Member m) throws LoginException {
-		
+	public Member loginMember(Member m) throws LoginException {		
 		return md.loginCheck(m);
 	}
-
+	
+	//회원 가입
 	@Override
 	public void insertMember(Member m) {
 		int result = md.insertMember(m, sqlSession);
+	}
+	
+	//관리자 페이지 회원 리스트 불러오기
+	@Override
+	public ArrayList<Member> selectMemberList() {
+		// TODO Auto-generated method stub
+		System.out.println("옴");
+		
+		ArrayList<Member> list = md.selectMemberList();
+		
+		System.out.println("list" + list);
+		
+		return list;
 	}
 }
