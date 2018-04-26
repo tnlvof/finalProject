719d855474b3d15a2ab5fd29b97499f2d212fd81
ksbin1 <chubbykwon@naver.com>
2018-04-26 17:59:56 +0900

    지은언니부분

--- Fooding/src/main/java/com/kh/fooding/sample/controller/SampleController.java
+++ Fooding/src/main/java/com/kh/fooding/sample/controller/SampleController.java
@@ -1,12 +1,8 @@
 package com.kh.fooding.sample.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.fooding.sample.model.service.SampleService;
